2418e6b123f428e9aa02944657aa8ccd833185c1
Billy-Desktop <wkarnchanapee@gmail.com>
2017-12-03 15:57:37 +0000

    Made kitchen counter/sink prefabs. also added foodcontrooller

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FoodController : MonoBehaviour
+{
+    public float progress = 0f;
+
+    public float salty = 0f;
+    public float sweet = 0f;
+    public float sour = 0f;
+    public float spicy = 0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
