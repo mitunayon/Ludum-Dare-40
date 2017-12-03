85d44be0bbbc8e954f61821a7a0c8ed9a236577b
KENNYPC\kenny <kennykr@hotmail.co.uk>
2017-12-03 20:59:17 +0000

    Changes to spawn

---
+++ Assets/Scripts/CustomerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CustomerController : MonoBehaviour {
+    private bool isHungry = true;
+    private NavMeshAgent agent;
+    [SerializeField]
+    private GameObject target;
+    private Renderer renderer;
+    private Collider collider;
+    private bool isSeated = false;
+    private Rigidbody rb;
+    private GameObject door; 
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+        agent.enabled = true;
+        target = GameObject.FindWithTag("empty seat");
+        agent.SetDestination(new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isSeated)
+        {
+            target = GameObject.FindWithTag("empty seat");
+            if (target == null)
+            {
+                target = GameObject.Find("Player");
+            }
+            agent.SetDestination(new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z));
+        }
+       
+        
+
+        if (isHungry) {
+            renderer.material.color = Color.red;
+            
+        }
+        else{
+            renderer.material.color = Color.green;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision){
+        GameObject seatGo = collision.gameObject;
+        if (seatGo == target && !isSeated && target != GameObject.Find("Player")) {
+            //stop movement
+            target.tag = "seat";
+            isSeated = true;
+            agent.isStopped = true;
+            agent.enabled = false;
+            collider.isTrigger = true;
+            rb.isKinematic = true;
+            //moves customer to seat
+            transform.position = seatGo.transform.Find("Sitting Position").transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "finished food" && isSeated) {
+            //restarts movement
+            Destroy(other.gameObject);
+            target.tag = "empty seat";
+            agent.enabled = true;
+            agent.isStopped = false;
+            collider.isTrigger = false;
+            rb.isKinematic = false;
+            isHungry = false;
+            //customer leaves
+            target = GameObject.Find("Customer Spawn");
+            agent.SetDestination(new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z));
+        }
+        
+    }
+
+
+}
