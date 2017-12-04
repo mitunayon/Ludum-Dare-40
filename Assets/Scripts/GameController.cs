b18c7f2f9e0d7c5e9381ac310f20d654710ecd7d
KENNYPC\kenny <kennykr@hotmail.co.uk>
2017-12-04 17:55:38 +0000

    Added meatChunk, grilledMeat and yuck variable gameobject

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,9 @@ public class GameController : MonoBehaviour {
     public string gameState = "live";
     public GameObject soupObj;
     public GameObject sandwichObj;
+    public GameObject meatChunksObj;
+    public GameObject grilledMeatObj;
+    public GameObject yuckObj;
     public GameObject[] ingredientReward;
     public GameObject player;
     public int money = 0;
