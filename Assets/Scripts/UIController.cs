9185277a4340064a1466588c7c650a1240913ab1
Billy-Desktop <wkarnchanapee@gmail.com>
2017-12-04 16:53:55 +0000

    Added money UI and UI structure

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public Text MoneyUI;
+    GameController gameCtrl;
+	// Use this for initialization
+	void Start () {
+        gameCtrl = GameObject.Find("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoneyUI.text = "Money: £"+gameCtrl.money.ToString();
+	}
+}
