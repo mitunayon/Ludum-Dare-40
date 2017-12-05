d32ebafb8b92139502d00f0e067edb4efab55d31
KENNYPC\kenny <kennykr@hotmail.co.uk>
2017-12-05 00:06:26 +0000

    Money is now neede to buy ingredients

--- Assets/Scripts/BuyController.cs
+++ Assets/Scripts/BuyController.cs
@@ -6,17 +6,19 @@ public class BuyController : MonoBehaviour {
     public bool pressed;
     public GameObject foodToSpawn;
     public float distanceFromButton;
+    public GameController gameController;
 	// Use this for initialization
 	void Start () {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
         pressed = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (pressed) {
-            Debug.Log("Works?");
+        if (pressed && gameController.money > 6) {
             SpawnFood(foodToSpawn);
             pressed = false;
+            gameController.money -= 5;
         }
 	}
     void SpawnFood(GameObject foodToSpawn) {
