3d7ac9b25f1320c65f93c9f4d5a07a5951ebb7fc
KENNYPC\kenny <kennykr@hotmail.co.uk>
2017-12-06 02:55:01 +0000

    Reworked Saucepan overhead bar

--- Assets/Scripts/OverheadHealthbarController.cs
+++ Assets/Scripts/OverheadHealthbarController.cs
@@ -1,40 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OverheadHealthbarController : MonoBehaviour
-{
-    SpriteRenderer rend;
-    
+public class OverheadHealthbarController : MonoBehaviour {
+    private SpriteRenderer rend;
+
     public Sprite[] healthbarSprites;
-    GameController gameCtrl;
-    CookingContainer objCtrl;
+    private GameController gameCtrl;
 
-    void Start()
-    {
-        rend = GetComponent<SpriteRenderer>();
-        objCtrl = GetComponentInParent<CookingContainer>();
-        gameCtrl = GameObject.Find("GameController").GetComponent<GameController>();
-    }
-    void Update()
-    {
+    protected void ChangeSprite(float progress) {
+        switch (gameCtrl.gameState) {
 
-        switch (gameCtrl.gameState)
-        {
-            
             case "live":
-
-                
-                float hpPerc = ((float)objCtrl.progress / 100f) * 100f;
+                float hpPerc = (progress / 100f) * 100f;
                 if (hpPerc > 100) hpPerc = 100f;
 
-				float indexDivider = 100 / (healthbarSprites.Length - 1);
-				int spriteIndex = (int) ((hpPerc + 2.5f) / indexDivider);
-				//print("spriteIndex " + spriteIndex);
-				rend.sprite = healthbarSprites[spriteIndex];
-                
+                float indexDivider = 100 / (healthbarSprites.Length - 1);
+                int spriteIndex = (int)((hpPerc + 2.5f) / indexDivider);
+                //print("spriteIndex " + spriteIndex);
+                rend.sprite = healthbarSprites[spriteIndex];
+
                 break;
         }
-        
-        
+
+    }
+    protected void Initialise() {
+        rend = GetComponent<SpriteRenderer>();
+        gameCtrl = GameObject.Find("GameController").GetComponent<GameController>();
     }
 }
---
+++ Assets/Scripts/SaucepanOverheadBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaucepanOverheadBar : OverheadHealthbarController {
+    CookingContainer objCtrl;
+    // Use this for initialization
+    void Start () {
+        Initialise();
+        objCtrl = GetComponentInParent<CookingContainer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ChangeSprite((float) objCtrl.progress);
+	}
+}
